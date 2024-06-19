16289c78204c5deb8fc7ba3fcc8847b3509ab4c3
Ladoxer <ladderdoxer@gmail.com>
2024-06-19 12:44:02 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { DropDownFilterComponent } from './components/drop-down-filter/drop-down-filter.component';
+import { ImagesComponent } from './components/images/images.component';
+import { SearchComponent } from './components/search/search.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumsComponent,
+    DropDownFilterComponent,
+    ImagesComponent,
+    SearchComponent,
+    SidebarComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/drop-down-filter/drop-down-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-down-filter',
+  templateUrl: './drop-down-filter.component.html',
+  styleUrl: './drop-down-filter.component.css'
+})
+export class DropDownFilterComponent {
+
+}
